9e4a62b40b06040a82fa389f94c52933dcebbe42
Malte Kyhos <malte.kyhos@civey.com>
2022-02-24 23:01:15 +0100

    Improve functions

--- R/functions.R
+++ R/functions.R
@@ -9,8 +9,16 @@
 #' - pacman
 #' @param scope The file to be read.
 #' @return character(n)
-find_used_packages <- function(scope) {
-  pkg_lines <- grep("^(library\\(|p_load\\()", readLines(scope), value = TRUE)
+find_used_packages <- function(file) {
+  # Check file name
+  if (!any(file == dir())) {
+    file_name <- grep(file, dir(), value = TRUE)[1]
+    if (length(file) == 0) stop(sprintf("File '%s' not found.", file))
+  } else {
+    file_name <- file
+  }
+
+  pkg_lines <- grep("^(library\\(|p_load\\()", readLines(file_name), value = TRUE)
   pkg_names <- vapply(pkg_lines, FUN.VALUE = character(1), FUN = function(x) {
     regmatches(x, gregexpr("(?<=\\().*(?=\\))", x, perl = TRUE))[[1]]
   })
@@ -25,11 +33,13 @@ find_used_packages <- function(scope) {
 #' Function to print a character vector of packages names in a nice way
 #' (max 3 packages in a row). Assumes that 'pacman' is used for loading / installing.
 #' @param packages character(n)
-print_used_packages <- function(packages) {
+#' @param rowlen numeric(1) How many package names should be printed per row?
+#' Defaults to 3.
+print_used_packages <- function(packages, rowlen = 3) {
   packages <- sprintf("\"%s\"", packages)
   wrapped <- character()
-  for (i in seq_len(ceiling(length(packages) / 3))) {
-    wrapped[i] <- toString(packages[(i * 3 - 2):min((i * 3), length(packages))])
+  for (i in seq_len(ceiling(length(packages) / rowlen))) {
+    wrapped[i] <- toString(packages[(i * rowlen - rowlen + 1):min((i * rowlen), length(packages))])
   }
 
   wrapped <- paste(wrapped, collapse = ",\n\t")
